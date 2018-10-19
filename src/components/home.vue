b6cf3bef7cf5d32c853c396cba585394a528c03c
yilinhai <495926158@qq.com>
2018-10-19 18:07:56 +0800

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header>
+        <div class="top">
+          <div class="l">
+            <img src="/static/images/portal_logo.png" alt="">
+            <span>欢迎您！管理员/admin</span>
+          </div>
+          <div class="r">
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                语言<i class="el-icon-arrow-down el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>中文</el-dropdown-item>
+                <el-dropdown-item>English</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+            <div class="exit">
+              <i class="el-icon-mobile-phone"></i>
+              <i class="el-icon-circle-close"></i>
+            </div>
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="l">
+            <span></span>
+          </div>
+          <div class="r">
+            <el-button type="primary large">未结算金额</el-button>
+          </div>
+        </div>
+      </el-header>
+      <el-container class="main">
+        <el-aside width="200px">
+          <el-menu :default-active="0" class="el-menu-vertical-demo"
+                   @open="handleOpen"
+                   @close="handleClose">
+            <el-menu-item index="1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">首页</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-mobile-phone"></i>
+              <span slot="title">用户管理</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-goods"></i>
+              <span slot="title">商家管理</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-sold-out"></i>
+              <span slot="title">交易管理</span>
+            </el-menu-item>
+            <el-menu-item index="5">
+              <i class="el-icon-document"></i>
+              <span slot="title">报表管理</span>
+            </el-menu-item>
+            <el-menu-item index="6">
+              <i class="el-icon-printer"></i>
+              <span slot="title">结算管理</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>今日新增商户数</span>
+              <div>0</div>
+            </div>
+            <div class="">
+              本周
+            </div>
+            <div>
+              0
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>今日交易金额</span>
+              <div>0</div>
+            </div>
+            <div class="">
+              本周
+            </div>
+            <div>
+              0
+            </div>
+          </el-card>
+        </el-main>
+      </el-container>
+      <el-footer>
+          <el-tag type="info">关于我们</el-tag>
+          <el-tag type="info">联系我们</el-tag>
+          <el-tag type="info">隐私政策</el-tag>
+          <el-tag type="info">服务条款</el-tag>
+      </el-footer>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home",
+    data() {
+      return {
+        msg: 'hello'
+
+      }
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .el-header{
+    height: 120px !important;
+    padding: 0;
+    .top {
+      background-color: #0091D7;
+      height: 60px;
+      padding: 0 40px;
+      display: flex;
+      justify-content: space-between;
+      .l{
+        line-height: 60px;
+        img{
+          height: 48px;
+          vertical-align: middle;
+        }
+        span{
+          margin-left: 10px;
+          color: #fff;
+          font-size: 16px;
+        }
+      }
+      .r{
+        .el-dropdown{
+          margin-top: 19px;
+          color: white;
+        }
+        .exit{
+          display: inline-block;
+          line-height: 60px;
+          margin-left: 20px;
+          font-size: 24px;
+          i {
+            margin-right: 10px;
+            cursor: pointer;
+            color: white;
+          }
+        }
+      }
+    }
+    .bottom{
+      height: 60px;
+      background-color: #f3f4f5;
+      padding: 0 40px;
+      display: flex;
+      justify-content: space-between;
+      .l{
+        line-height: 60px;
+        margin-left: 60px;
+        img{
+          height: 48px;
+          vertical-align: middle;
+        }
+        span{
+          margin-left: 10px;
+          color: #333;
+          font-size: 16px;
+        }
+      }
+      .r{
+        line-height: 60px;
+      }
+    }
+  }
+
+  .main{
+    width: 1200px;
+    margin: 0 auto;
+  }
+
+  .box-card {
+    width: 450px;
+    margin: 0 10px;
+    float: left;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: left;
+  }
+
+  .el-footer{
+    width: 100%;
+    margin-top: 90px;
+    background-color: #fff;
+    line-height: 60px;
+    text-align: center;
+  }
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="login">
+    <header class="header">
+      <div class="logo">
+        <img src="/static/images/portal_logo.png" alt="logo">
+        <span>{{logo_title}}</span>
+      </div>
+      <div class="language">
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            {{language}}<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>中文</el-dropdown-item>
+            <el-dropdown-item>English</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </header>
+    <!--container布局容器-->
+    <el-container>
+      <el-header></el-header>
+      <el-main>
+        <div class="login-part">
+          <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+            <h4>代理商登录</h4>
+            <el-form-item label="" prop="loginId">
+              <el-input v-model="ruleForm.loginId" placeholder="您的代理商编号"></el-input>
+            </el-form-item>
+            <el-form-item label="" prop="name">
+              <el-input v-model="ruleForm.name" placeholder="您的登录名"></el-input>
+            </el-form-item>
+            <el-form-item label="" prop="pwd">
+              <el-input v-model="ruleForm.pwd" placeholder="请输入登录密码" type="password"></el-input>
+            </el-form-item>
+            <el-form-item label="记住我" class="remember-me">
+              <el-switch v-model="ruleForm.rememberMe"></el-switch>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary large" @click="submitForm('ruleForm')" class="login-btn">登录</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        logo_title: '代理商后台管理系统',
+        language: '语言',
+        ruleForm: {
+          loginId: '',
+          name: '',
+          pwd: '',
+          rememberMe: false
+        },
+        rules: {
+          loginId: [
+            {required: true, message: '请输入您的代理商编号', trigger: 'blur'},
+            {min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+          ],
+          name: [
+            {required: true, message: '请输入您的登录名', trigger: 'blur'},
+            {min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+          ],
+          pwd: [
+            {required: true, message: '请输入登录密码', trigger: 'blur'},
+            {min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+          ],
+        },
+      }
+    },
+    methods: {
+      submitForm(formName){
+        this.$refs[formName].validate((valid) => {
+          if (valid){
+            alert('login')
+            this.$router.push({name: 'home'})
+          }else {
+            console.log('error submit')
+            return false;
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .header {
+    width: 1200px;
+    height: 95px;
+    display: flex;
+    padding: 5px;
+    margin: 0 auto;
+    justify-content: space-between;
+    .logo{
+      line-height: 95px;
+      img{
+        vertical-align: middle;
+      }
+      span{
+        margin-left: 10px;
+        color: #0091D7;
+        font-size: 20px;
+      }
+    }
+    .language{
+      .el-dropdown{
+        margin-top: 39px;
+      }
+    }
+  }
+
+  .el-header {
+    background-color: #0091D7;
+  }
+
+  .el-main{
+    position: relative;
+    width: 1200px;
+    margin: 0 auto;
+    height: 600px;
+    background: url(/static/images/login_pic.png) no-repeat;
+    background-size: 100%;
+    .login-part{
+      position: absolute;
+      left: 50%;
+      top: 35%;
+      margin: -193px 0 0 -143px;
+      background: rgba(255, 255, 255, 0.9);
+      border-radius: 5px;
+      padding: 20px 30px;
+      h4{
+        font-size: 18px;
+        color: #696969;
+      }
+      .remember-me{
+        margin-bottom: 0;
+      }
+      .login-btn{
+        width: 240px;
+      }
+    }
+  }
+</style>
